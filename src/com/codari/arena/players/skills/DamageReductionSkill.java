b4367ebe996d057dfd208602ae34f3656a73e3b8
Mhenlo <mphils55@gmail.com>
2013-11-18 19:05:21 -0500

    Damage Reduction Skill finished!

--- src/com/codari/arena/players/skills/DamageReductionSkill.java
+++ src/com/codari/arena/players/skills/DamageReductionSkill.java
@@ -1,26 +1,66 @@
 package com.codari.arena.players.skills;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import com.codari.api5.Codari;
 import com.codari.arena5.players.combatants.Combatant;
 import com.codari.arena5.players.skills.Skill;
 import com.codari.arena5.players.skills.SkillActivation;
+import com.codari.arena5.players.teams.Team;
 
 public class DamageReductionSkill implements Skill {
 	//-----Fields-----//
 	private int effectDuration = 40;
 	private int effectAmplification = 10;
+	
+	private int damageRadius = 5;
+	private int damageDoneToNearbyEnemies = 10;
 
 	//TODO - Deal damage to enemies nearby
 	@Override
 	public void activateSkill(Combatant combatant) {
+		//Apply Potion Effect for damage resistance
 		PotionEffect damageResistancePotionEffect = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, effectDuration, effectAmplification);
 		damageResistancePotionEffect.apply(combatant.getPlayerReference().getPlayer());
+		
+		//Apply damage to nearby enemies
+		Player player = combatant.getPlayerReference().getPlayer();
+		Team team = Codari.INSTANCE.getArenaManager().getTeam(combatant);
+		List<Entity> entities = player.getNearbyEntities(damageRadius, damageRadius, damageRadius);
+		List<Player> players = this.editList(entities);
+		if(players.size() > 1) {
+			for(Player targetPlayer: players) {
+				if(Codari.INSTANCE.getArenaManager().getTeam(Codari.INSTANCE.getArenaManager().getCombatant(targetPlayer)) != team) {
+					double currentHealth = targetPlayer.getHealth();
+					targetPlayer.setHealth(currentHealth - damageDoneToNearbyEnemies);
+				} 
+			}
+		}
 	}
 
 	@Override
 	public SkillActivation getSkillActivation() {
 		return SkillActivation.BLOCK;
+	}
+	
+	private List<Player> editList(List<Entity> entities) { 
+		List<Player> players = new ArrayList<Player>();
+		Iterator<Entity> iterator = entities.iterator();
+		while(iterator.hasNext()) {
+			Entity entity = iterator.next();
+			if(!(entity instanceof Player)) {
+				iterator.remove();
+			} else { 
+				players.add((Player)entity);
+			}
+		}
+		return players;
 	}
 }
