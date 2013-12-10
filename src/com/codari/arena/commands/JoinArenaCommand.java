8e7e4d517c568b94fa709ef2308f75fea0cf9226
Diage <farina.1919@gmail.com>
2013-12-10 11:57:35 -0500

    Left you plenty of comments

--- src/com/codari/arena/commands/JoinArenaCommand.java
+++ src/com/codari/arena/commands/JoinArenaCommand.java
@@ -12,9 +12,25 @@ import com.codari.api5.Codari;
 import com.codari.arena5.Arena;
 import com.codari.arena5.players.teams.Team;
 
+/*
+ * Need to create interim object called a 'Queue' - Simple implementation is easy, you pass in an arena to the constructor
+ * and it just gets the number of teams required for that arena and potentially the team size. Once the number of teams in 
+ * the queue is equal to the number required, it begins the arena and the arena can check a lot of the stuff for us. 
+ * 
+ * A more complicated implementation is to do a lot of the checks within the queue in which case you will either need to copy over
+ * variables from the arena or save a reference to the arena. You will most likely need a queue manager within arena manager which ties
+ * a queue to an ArenaName. 
+ * 
+ * In either case, the CommandSender ought to only check the parameters passed in to it and then do nothing else, let all the other objects
+ * do those. Basically just check if a queue with the provided name exists, if so, try to add to queue and let the queue do the rest.
+ * 
+ * Whether you put the checks in the queue or the Arena Start is not terribly relevant at the moment. 
+ */
+
 public class JoinArenaCommand implements CommandExecutor {
 
 	/* TWO PROBLEMS: Need a method to set teams in the arena and a way to set an arena for a team. 
+	 * 	--Arena.start(Team...teams) and listen for an event to fire for arena start.
 	 * Right now getting a map of teams in an arena only returns a copy. Need a way to actually set teams for an arena. */
 	
 	@Override
@@ -37,7 +53,7 @@ public class JoinArenaCommand implements CommandExecutor {
 					}
 					return true;
 				} else {																//if there is a team already waiting
-					arena.getTeams().put(team.getTeamName(), team);
+					arena.getTeams().put(team.getTeamName(), team);	//This won't work at all. I'm sure you've figured that out...
 					for(Map.Entry<String, Team> teamEntry: arena.getTeams().entrySet()) {
 						Team arenaTeam = teamEntry.getValue();
 						for(Player teamPlayer : arenaTeam.getPlayers()) {
