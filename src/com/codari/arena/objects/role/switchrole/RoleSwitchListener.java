751410df39fe0dab6aec79ba0a8bbd994511ab01
Mhenlo <mphils55@gmail.com>
2013-12-10 21:15:03 -0500

    debugging

--- src/com/codari/arena/objects/role/switchrole/RoleSwitchListener.java
+++ src/com/codari/arena/objects/role/switchrole/RoleSwitchListener.java
@@ -1,5 +1,6 @@
 package com.codari.arena.objects.role.switchrole;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -63,6 +64,9 @@ public class RoleSwitchListener implements Listener {
 	public void onPlayerJoinArena(ArenaStartEvent e) {
 		for(String teamName : e.getArena().getTeams().keySet()) {	//TODO
 			Team team = e.getArena().getTeams().get(teamName);
+			if(team == null) {
+				Bukkit.broadcastMessage(ChatColor.RED + "Team is null!");
+			}
 			for(Player player : team.getPlayers()) {
 				RoleSwitch.createRoleSwitchObject(Codari.getArenaManager().getCombatant(player));
 			}
