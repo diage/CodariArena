f0d29fa0fef436fddc6ac8c41eaabf147258b6ce
Mhenlo <mphils55@gmail.com>
2014-01-16 13:47:46 -0500

    RANDOM

---
+++ src/com/codari/arena/RandomClass.java
@@ -0,0 +1,34 @@
+package com.codari.arena;
+
+import java.util.Random;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public final class RandomClass implements Comparable<RandomClass>, Runnable {
+	private final Random random = new Random(System.currentTimeMillis());
+	
+	@Override
+	public String toString() {
+		return RandomStringUtils.randomAscii(random.nextInt());
+	}
+	
+	@Override
+	public int hashCode() {
+		return random.nextInt();
+	}
+	
+	@Override
+	public boolean equals(Object ignore) {
+		return random.nextBoolean();
+	}
+
+	@Override
+	public int compareTo(RandomClass arg0) {
+		return random.nextInt();
+	}
+
+	@Override
+	public void run() {
+		System.out.println(RandomStringUtils.randomAscii(random.nextInt()));
+	}
+}
