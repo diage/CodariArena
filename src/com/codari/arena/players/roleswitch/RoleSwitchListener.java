f2472d5d12dba99d3a0711678fff9d2698cf905e
Mhenlo <mphils55@gmail.com>
2014-01-18 14:44:23 -0500

    fixed stuff

--- src/com/codari/arena/players/roleswitch/RoleSwitchListener.java
+++ src/com/codari/arena/players/roleswitch/RoleSwitchListener.java
@@ -1,5 +1,7 @@
 package com.codari.arena.players.roleswitch;
 
+import java.util.Map.Entry;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,13 +14,10 @@ import com.codari.arena5.players.teams.Team;
 public class RoleSwitchListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerJoinArena(ArenaStartEvent e) {
-		for(String teamName : e.getArena().getTeams().keySet()) {	//FIXME - fix the iteration through the keyset
-			Team team = e.getArena().getTeams().get(teamName);	
+		for(Entry<String, Team> teams : e.getArena().getTeams().entrySet()) {	
+			Team team = teams.getValue();
 			team.combatants().get(0).setRole(Codari.getArenaManager().getExistingRole(ArenaStatics.ARENA_NAME, ArenaStatics.MELEE));
 			team.combatants().get(1).setRole(Codari.getArenaManager().getExistingRole(ArenaStatics.ARENA_NAME, ArenaStatics.RANGED));			
-			//for(Player player : team.getPlayers()) {			TODO
-				//RoleSwitch.createRoleSwitchObject(Codari.getArenaManager().getCombatant(player));
-			//}
 		}
 	}
 }
