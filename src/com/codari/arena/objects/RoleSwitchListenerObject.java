04cda1f447c2b25e982a8c5e3d1afd38fd36e95c
Mhenlo <mphils55@gmail.com>
2014-01-26 22:31:01 -0500

    Updated

--- src/com/codari/arena/objects/RoleSwitchListenerObject.java
+++ src/com/codari/arena/objects/RoleSwitchListenerObject.java
@@ -1,56 +1,54 @@
 package com.codari.arena.objects;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 
 import com.codari.api5.CodariI;
 import com.codari.arena.ArenaStatics;
+import com.codari.arena.players.RoleHotbarListener;
 import com.codari.arena.players.roleswitch.RoleObjectItemTypes;
 import com.codari.arena5.objects.ArenaObjectName;
 import com.codari.arena5.objects.spawnable.ListenerFixedSpawnableObject;
+import com.codari.arena5.players.combatants.Combatant;
+import com.codari.arena5.players.hotbar.HotbarSelectEvent;
 import com.codari.arena5.players.role.RoleSelectEvent;
 
 @ArenaObjectName(value = "Role Switch Listener (2v2)", links = {ArenaStatics.MELEE, ArenaStatics.RANGED})
 public class RoleSwitchListenerObject implements ListenerFixedSpawnableObject {
 	private static final long serialVersionUID = -1837467493766979324L;
+	private final Enchantment enchantment = Enchantment.SILK_TOUCH;
+	private final Map<String, String> approveRoleSwitch;
 
 	private Location location;
 	private transient BlockState glassBlockState;
 	private Material glassMaterial = Material.GLASS;
 	private String name, arenaName;
+	private boolean enabled;
 
 	public RoleSwitchListenerObject(Location location) {
 		this.location = location;
 		this.glassBlockState = location.getBlock().getState();
 		ArenaObjectName objectName = this.getClass().getAnnotation(ArenaObjectName.class);
 		this.name = objectName.value();
+		this.approveRoleSwitch = new HashMap<>();
 	}
 
-	@SuppressWarnings("deprecation")
-	@EventHandler()
-	private void playerSwapEvent(RoleSelectEvent e) {	
-		Bukkit.broadcastMessage(ChatColor.GREEN + "Role Select Event heard!");	//TODO
-		if(e.wasSwap()) {
-			e.getCombatant().getPlayer().sendMessage(ChatColor.GREEN + "Your role is now " + ChatColor.DARK_GREEN + e.getNewRole().getName());
-		} 
-		if(e.getNewRole().getLink(this.name).equalsIgnoreCase(ArenaStatics.MELEE)) {	
-			e.getCombatant().getPlayer().getInventory().setItem(0, RoleObjectItemTypes.MELEE.getItemStack());
-			e.getCombatant().getPlayer().updateInventory();
-		} else if(e.getNewRole().getLink(this.name).equals(ArenaStatics.RANGED)) {	
-			e.getCombatant().getPlayer().getInventory().setItem(0, RoleObjectItemTypes.RANGED.getItemStack());
-			e.getCombatant().getPlayer().updateInventory();
-		}	
-	}
-	
 	@Override
 	public void spawn() {
-		if(this.arenaName != null) {
+		if(this.arenaName != null && !this.enabled) {
 			Bukkit.getPluginManager().registerEvents(this, CodariI.INSTANCE);
+			this.enabled = true;
 		}
 	}
 
@@ -62,7 +60,10 @@ public class RoleSwitchListenerObject implements ListenerFixedSpawnableObject {
 	@Override
 	public void hide() {
 		this.glassBlockState.update(true);
-		HandlerList.unregisterAll(this);
+		if(this.enabled) {
+			HandlerList.unregisterAll(this);
+			this.enabled = false;
+		}
 	}
 
 	@Override
@@ -78,5 +79,84 @@ public class RoleSwitchListenerObject implements ListenerFixedSpawnableObject {
 
 	public void setArenaName(String arenaName) {
 		this.arenaName = arenaName;
+	}
+
+	//---Role Switching---//
+	@EventHandler()
+	private void playerSwapEvent(RoleSelectEvent e) {	
+		if(e.getCombatant().getArenaName() != null && e.getCombatant().getArenaName().equals(this.arenaName)) {
+			if(e.wasSwap()) {
+				e.getCombatant().getPlayer().sendMessage(ChatColor.GREEN + "Your role is now " + ChatColor.DARK_GREEN + e.getNewRole().getName());
+			} 
+			if(e.getNewRole().getLink(this.name).equalsIgnoreCase(ArenaStatics.MELEE)) {	
+				this.updateMeleeIcon(e.getCombatant().getPlayer());
+			} else if(e.getNewRole().getLink(this.name).equals(ArenaStatics.RANGED)) {	
+				this.updateRangedIcon(e.getCombatant().getPlayer());
+			}	
+		}
+	}	
+
+	@EventHandler
+	private void hotbardom(HotbarSelectEvent e) {
+		if(e.getCombatant().inArena() && e.getCombatant().getArenaName().equals(this.arenaName)) {
+			switch (e.getOption()) {
+			case HOTBAR_1:
+				this.activateRoleSwitch(e);
+				break;
+			default:
+				return;
+			}
+			e.getCombatant().setHotbarCooldown(RoleHotbarListener.GLOBAL_COOLDOWN);	
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private void activateRoleSwitch(HotbarSelectEvent e) {
+		if(e.getCombatant().inArena() && e.getCombatant().getArenaName().equals(this.arenaName)) {
+			Player player, teamMatePlayer;
+			Combatant combatant, teamMateCombatant;
+			int slotNumber = e.getOption().getInventorySlot();
+
+			combatant = e.getCombatant();
+			teamMateCombatant = combatant.getTeam().getTeamMates(combatant).get(0);
+			if(teamMateCombatant != null) {
+				player = e.getCombatant().getPlayer();
+				teamMatePlayer = teamMateCombatant.getPlayer();
+			} else {
+				Bukkit.broadcastMessage(ChatColor.DARK_RED + "INVALID TEAMS!");
+				return;
+			}
+
+			String teamName = combatant.getTeam().getTeamName();
+			if(this.approveRoleSwitch.containsKey(teamName)) {
+				if(player.getName() != this.approveRoleSwitch.get(teamName)) {
+					teamMateCombatant.swapRole(combatant.swapRole(teamMateCombatant.getRole())); 
+					this.approveRoleSwitch.remove(teamName);
+				} else {
+					player.sendMessage(ChatColor.AQUA + "You have already requested a switch.");
+				}
+			} else {
+				ItemStack teamMateRoleSwitchItem;
+				teamMateRoleSwitchItem = teamMatePlayer.getInventory().getItem(slotNumber);
+				teamMateRoleSwitchItem.addUnsafeEnchantment(this.enchantment, 1);
+
+				this.approveRoleSwitch.put(teamName, player.getName());
+				teamMatePlayer.sendMessage(ChatColor.AQUA + "Your teammate is requesting a role switch.");
+			}
+			teamMatePlayer.updateInventory();
+			player.updateInventory();
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private void updateMeleeIcon(Player player) {
+		player.getInventory().setItem(0, RoleObjectItemTypes.MELEE.getItemStack());
+		player.updateInventory();
+	}
+
+	@SuppressWarnings("deprecation")
+	private void updateRangedIcon(Player player) {
+		player.getInventory().setItem(0, RoleObjectItemTypes.RANGED.getItemStack());
+		player.updateInventory();
 	}
 }
--- src/com/codari/arena/players/RoleHotbarListener.java
+++ src/com/codari/arena/players/RoleHotbarListener.java
@@ -1,38 +1,22 @@
 package com.codari.arena.players;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.Potion;
 
 import com.codari.api5.util.BukkitUtils;
 import com.codari.api5.util.scheduler.BukkitTime;
-import com.codari.arena5.players.combatants.Combatant;
 import com.codari.arena5.players.hotbar.HotbarSelectEvent;
 
 public class RoleHotbarListener implements Listener {
-	private final static long GLOBAL_COOLDOWN = BukkitTime.SECOND.tickValueOf(1);
-	private final Enchantment enchantment = Enchantment.SILK_TOUCH;
-	private final Map<String, String> approveRoleSwitch;
-
-	public RoleHotbarListener() {
-		this.approveRoleSwitch = new HashMap<>();
-	}
+	public final static long GLOBAL_COOLDOWN = BukkitTime.SECOND.tickValueOf(1);
 
 	@EventHandler
 	private void hotbardom(HotbarSelectEvent e) {
 		if (e.getCombatant().inArena()) {
 			switch (e.getOption()) {
 			case HOTBAR_1:
-				this.activateRoleSwitch(e);
-				break;
+				return;	//Moved to RoleSwitchListenerObject
 			case HOTBAR_2:
 			case HOTBAR_3:
 			case HOTBAR_4:
@@ -56,39 +40,4 @@ public class RoleHotbarListener implements Listener {
 		}
 	}
 
-	@SuppressWarnings("deprecation")
-	private void activateRoleSwitch(HotbarSelectEvent e) {
-		Player player, teamMatePlayer;
-		Combatant combatant, teamMateCombatant;
-		int slotNumber = e.getOption().getInventorySlot();
-
-		combatant = e.getCombatant();
-		teamMateCombatant = combatant.getTeam().getTeamMates(combatant).get(0);
-		if(teamMateCombatant != null) {
-			player = e.getCombatant().getPlayer();
-			teamMatePlayer = teamMateCombatant.getPlayer();
-		} else {
-			Bukkit.broadcastMessage(ChatColor.DARK_RED + "INVALID TEAMS!");
-			return;
-		}
-
-		String teamName = combatant.getTeam().getTeamName();
-		if(this.approveRoleSwitch.containsKey(teamName)) {
-			if(player.getName() != this.approveRoleSwitch.get(teamName)) {
-				teamMateCombatant.swapRole(combatant.swapRole(teamMateCombatant.getRole())); 
-				this.approveRoleSwitch.remove(teamName);
-			} else {
-				player.sendMessage(ChatColor.AQUA + "You have already requested a switch.");
-			}
-		} else {
-			ItemStack teamMateRoleSwitchItem;
-			teamMateRoleSwitchItem = teamMatePlayer.getInventory().getItem(slotNumber);
-			teamMateRoleSwitchItem.addUnsafeEnchantment(this.enchantment, 1);
-
-			this.approveRoleSwitch.put(teamName, player.getName());
-			teamMatePlayer.sendMessage(ChatColor.AQUA + "Your teammate is requesting a role switch.");
-		}
-		teamMatePlayer.updateInventory();
-		player.updateInventory();
-	}
 }
